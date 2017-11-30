16d08821ba7a19d318ad518e13a6cfff147914bc
anusha.s <anusha.s@zohocorp.com>
2017-11-30 23:52:18 +0530

    [first ex]

---
+++ ex1/src/ex12.java
@@ -0,0 +1,14 @@
+public class ex12
+{
+	public static void main(String [] args) throws Exception
+	{
+		try
+		{
+			System.out.println("*");
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception:" + e);
+		}
+	}
+}
